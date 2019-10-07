4968f6cdb61b3f252d848c3d5e07a5bb839d3244
Fabrice LEFEBVRE <fab.lefebvre@gmail.com>
2019-10-07 13:53:31 +0200

    Adding bootstrap-vue

--- food-compare-fe/src/main.js
+++ food-compare-fe/src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+import "./styles/global.scss";
 import App from "./App.vue";
 import router from "./router";
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
